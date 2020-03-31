256364fbaed9b9ac40680eccbb200549989237fc
savokin <locovname@gmail.com>
2020-03-30 19:14:26 -0700

    h

---
+++ script/sc.js
@@ -0,0 +1,28 @@
+let light = "greem";
+if(light == "greem"){
+    console.log("go");
+}else if(light == "yellow"){
+    console.log("ccautin");
+}else {
+    console.log("stop");
+}
+let bao = "tolerant";
+let kris = "intolerant";
+if(bao == "tolerant" && kris == "intolerant"){
+    console.log(" milk and almons milk coffe");
+}else if(bao =="intolerant" &&  kris == "intolerant"){
+    console.log("only almone");
+}else {
+    console.log("milk only");
+}
+let thermostat = 69;
+let funny_number = "xd";
+if(thermostat < 68){
+    console.log("cold");
+}else if(thermostat <= 72 && thermostat >= 68){
+    console.log("perfect");
+}else if(thermostat >=73 && thermostat <=76){
+    console.log("warm");
+}else {
+    console.log("hot");
+}
