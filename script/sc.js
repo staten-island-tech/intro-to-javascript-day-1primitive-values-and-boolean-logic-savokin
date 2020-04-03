9d66ce2d34856f2e3f92ce53628745cafedf3109
savokin <locovname@gmail.com>
2020-04-03 12:07:28 -0700

    sd
    a

--- script/sc.js
+++ script/sc.js
@@ -26,3 +26,40 @@ if(thermostat < 68){
 }else {
     console.log("hot");
 }
+
+
+let students = ["Riya", "Omor", "Alan", "Jaiyu", "Michael", "Lisa",
+"Richard"];
+let students_new = [];
+const bruh = function(...words){
+    words.forEach(word => {
+        if(word.length >= 5){
+            students_new.push(word);
+        }
+    });
+    console.log(...students_new);
+}
+bruh(...students);
+
+let students_2 = ["Riya", "Omor", "Alan", "Jaiyu", "Michael", "Lisa",
+"Richard"];
+let found = false;
+const compare = function(keyword, students){
+    
+    students.forEach(student => {
+        
+        if(student == keyword){
+            found = true;
+            
+        }
+    });
+    if(found == true){
+        console.log("found " + keyword);
+    }else {
+        console.log("nothing found");
+    }
+}
+
+compare("junshen" , students_2);
+compare("Savva", students_2);
+compare("Alan", students_2);
